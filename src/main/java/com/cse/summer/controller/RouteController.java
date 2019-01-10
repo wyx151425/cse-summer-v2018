0bd728c7fe9498ba2d77eebe36d6df66c0118b0c
wyx151425 <wyx151425@foxmail.com>
2019-01-10 11:41:38 +0800

    2019/01/10 完成机器结构分析界面

---
+++ src/main/java/com/cse/summer/controller/RouteController.java
@@ -0,0 +1,19 @@
+package com.cse.summer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author 王振琦
+ * createAt 2018/10/22
+ * updateAt 2018/10/22
+ */
+@Controller
+public class RouteController extends BaseFacade {
+
+    @RequestMapping(value = "logout")
+    public String routeLogin() {
+        removeSessionUser();
+        return "login";
+    }
+}
